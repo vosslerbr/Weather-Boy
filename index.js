b90fcfc469490456e1e7c5f35a72f76aa8c68fb7
Brady Vossler <bradyvosslerwebdev@gmail.com>
2021-03-16 16:15:10 +0000

    added landing page

--- index.js
+++ index.js
@@ -48,6 +48,30 @@ client.on("message", (msg) => {
 		})
   }
 
+	// TESTING EMBEDS
+	// if (msg.content.startsWith("$test")) {
+	// 	// inside a command, event listener, etc.
+	// 	const exampleEmbed = new Discord.MessageEmbed()
+	// 		.setColor('#ffffff')
+	// 		.setTitle('Some title')
+	// 		.setURL('https://discord.js.org/')
+	// 		.setAuthor('Weather Boy', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	// 		.setDescription('Some description here')
+	// 		.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	// 		.addFields(
+	// 			{ name: 'Regular field title', value: 'Some value here' },
+	// 			{ name: '\u200B', value: '\u200B' },
+	// 			{ name: 'Inline field title', value: 'Some value here', inline: true },
+	// 			{ name: 'Inline field title', value: 'Some value here', inline: true },
+	// 		)
+	// 		.addField('Inline field title', 'Some value here', true)
+	// 		.setImage('https://i.imgur.com/wSTFkRM.png')
+	// 		.setTimestamp()
+	// 		.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+	// 	msg.channel.send(exampleEmbed);
+	// }
+
 });
 
 keepAlive();
